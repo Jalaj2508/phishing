8236b9cb2029177f4844cb0adfa29bd2d5ed347f
Jalaj2508 <122887050+Jalaj2508@users.noreply.github.com>
2025-06-28 23:52:18 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,46 @@
-from flask import Flask
+#importing required libraries
+
+from flask import Flask, request, render_template
+import numpy as np
+import pandas as pd
+from sklearn import metrics 
+import warnings
+import pickle
+warnings.filterwarnings('ignore')
+from feature import FeatureExtraction
+
+file = open("pickle/model.pkl","rb")
+gbc = pickle.load(file)
+file.close()
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
 
-@app.route('/about')
-def about():
-    return 'About'
+        url = request.form["url"]
+
+        #Whitelist Websites
+        whitelist = ['google.com', 'microsoft.com', 'github.com']
+        if any(kw in url for kw in whitelist):
+            return render_template('index.html', xx=1.0, url=url)
+
+        #ML-based prediction
+        obj = FeatureExtraction(url)
+        x = np.array(obj.getFeaturesList()).reshape(1,30) 
+
+        y_pred =gbc.predict(x)[0]
+        #1 is safe       
+        #-1 is unsafe
+        y_pro_phishing = gbc.predict_proba(x)[0,0]
+        y_pro_non_phishing = gbc.predict_proba(x)[0,1]
+        # if(y_pred ==1 ):
+        pred = "It is {0:.2f} % safe to go ".format(y_pro_non_phishing * 100)
+        return render_template('index.html',xx =round(y_pro_non_phishing,2),url=url )
+    return render_template("index.html", xx =-1)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
